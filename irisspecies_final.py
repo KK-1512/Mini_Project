206776d50eb25898386cb6fd1b516b684dd0c800
Krishnakumar V <krishna.v15122004@gmail.com>
2025-04-27 17:12:06 +0530

    Update irisspecies_final.py

--- irisspecies_final.py
+++ irisspecies_final.py
@@ -1,50 +1,48 @@
 import streamlit as st
 import pickle
 
-
-
+# Functions to get user input
 def get_hardness():
-    hardness = st.text_input("HARDNESS")
-    return hardness
+    return st.text_input("HARDNESS")
 
 def get_toughness():
-    toughness = st.text_input("TOUGHNESS")
-    return toughness
+    return st.text_input("TOUGHNESS")
 
 def get_density():
-    density = st.text_input("DENSITY")
-    return density
+    return st.text_input("DENSITY")
 
 def get_yield_stress():
-    yield_stress = st.text_input("YIELD STRESS")
-    return yield_stress
+    return st.text_input("YIELD STRESS")
 
-
-
-def predict_material(h,t,d,y):
-    loaded_model = pickle.load(open('mini_project_model.pkl','rb'))
-    new_data = [[float(h),float(t),float(d),float(y)]]
+# Function to make prediction
+def predict_material(h, t, d, y):
+    loaded_model = pickle.load(open('mini_project_model.pkl', 'rb'))
+    new_data = [[float(h), float(t), float(d), float(y)]]
     prediction = loaded_model.predict(new_data)
-    st.write("Prediction with new data: ")
+    st.write("Prediction with new data:")
     st.write(prediction)
-    
 
+# Streamlit App
+st.title('Material Selection')
+st.image('app.png')
 
+hardness = get_hardness()
+toughness = get_toughness()
+density = get_density()
+yield_stress = get_yield_stress()
 
-if __name__ == "__main__":
-    st.title('Material Selection')
-    st.image('app.png')
-    hardness = get_hardness()
-    toughness = get_toughness()
-    density = get_density()
-    yield_stress = get_yield_stress()
-   
-    st.write("The parameters you entered are: ")
-    st.write("HARDNESS ", hardness)
-    st.write("TOUGHNESS ", toughness)
-    st.write("DENSITY ", density)
-    st.write("YIELD STRESS", yield_stress)
-    
+st.write("The parameters you entered are:")
+st.write("HARDNESS:", hardness)
+st.write("TOUGHNESS:", toughness)
+st.write("DENSITY:", density)
+st.write("YIELD STRESS:", yield_stress)
+
+if st.button("Predict"):
+    if hardness and toughness and density and yield_stress:
+        predict_material(hardness, toughness, density, yield_stress)
+    else:
+        st.warning("Please fill in all input fields before predicting.")
+
     
 
 
